15b2ed5c1ef4152d2ed866896c91a5b976fc74a8
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-04 05:19:28 +0700

    add category document model

---
+++ models/category.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+    const Schema = sequelize.define('kategori', {
+        id: {
+            primaryKey: true,
+            type: DataTypes.STRING,
+            unique: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        desc: {
+            type: DataTypes.TEXT,
+        },
+        fk_table_name: {
+            type: DataTypes.STRING,
+        },
+        icon: {
+            type: DataTypes.STRING,
+            allowNull:false,
+        },
+        dir_location: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        is_active: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0
+        }
+    }, 
+    {
+        createdAt: 'CreatedDate',
+        updatedAt: 'LastUpdated',
+        // timestamps: false,
+        freezeTableName: true,
+    });
+    // Schema.associate = (models) => {};
+    return Schema;
+};
