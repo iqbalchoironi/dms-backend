a441cdc0c5b9cacbd22ed5dd18fdcc9f452d86d5
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-03 21:28:04 +0700

    add roel user comtroller

---
+++ controllers/role_user_controller.js
@@ -0,0 +1,72 @@
+const Role = require('../models').role_Role;
+
+module.exports = {
+
+    read: async(req, res) => {
+
+        try {
+            let data = await Role.findAll({limit:parseInt(req.query.limit)});
+            res.json(data);
+        } catch(error){
+            console.error(error);
+        }
+    },
+
+    create: async(req, res) => {
+
+        try {
+
+            let valiableRole = await Role.findOne({where:{ name: req.body.name}});
+
+            if (valiableRole) {
+                return res.status(404).send('udah ada Rolename lu sob');
+            }
+
+            let newRole = await Role.create(req.body);
+
+            if (newRole) {
+                res.status(200).json(newRole);
+            }
+
+        }catch(error) {
+            console.log(error)
+        }
+    },
+
+    update: async(req, res) => {
+
+        try {
+
+            let valiableRole = await Role.findOne({where:{ id: req.params.id}});
+        
+            if (!valiableRole) {
+                return res.status(404).send('gak ada sob');
+            }
+            
+            await valiableRole.update(req.body);
+            res.status(200).json(valiableRole);
+
+        }catch(error) {
+
+        }
+        
+
+    },
+
+    remove: async(req, res) => {
+        try {
+
+            let valiableRole = await Role.findOne({where:{ id: req.params.id}});
+        
+            if (!valiableRole) {
+                return res.status(404).send('gak ada sob');
+            }
+    
+            await valiableRole.destroy();
+            res.status(200).send('udah di delete');
+
+        }catch(error) {
+            
+        }
+    }
+}
