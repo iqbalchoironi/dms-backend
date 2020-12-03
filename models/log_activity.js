cd870c261dbb75baf29bc4e7e2458362e831da32
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-04 06:06:48 +0700

    add log activity model

---
+++ models/log_activity.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+    const Schema = sequelize.define('loc_activity', {
+        id: {
+            primaryKey: true,
+            type: DataTypes.STRING,
+            unique: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        fk_username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        activity_type: {
+            type: DataTypes.STRING,
+        },
+        activity_object: {
+            type: DataTypes.STRING,
+        },
+        activity_object_detil: {
+            type: DataTypes.STRING,
+        },
+        activity_desc: {
+            type: DataTypes.STRING,
+        },
+        activity_time: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+    }, 
+    {
+        // createdAt: 'CreatedDate',
+        // updatedAt: 'LastUpdated',
+        timestamps: false,
+        freezeTableName: true,
+    });
+    // Schema.associate = (models) => {};
+    return Schema;
+};
