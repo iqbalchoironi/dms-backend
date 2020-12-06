4d41bd72ac815ba2f01ec584ad180340786d3b14
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-07 04:46:41 +0700

    add req.query limit and page in read spm

--- controllers/spm_controller.js
+++ controllers/spm_controller.js
@@ -1,16 +1,78 @@
 const Spm = require('../models').dok_spm;
-
+const { Op } = require('sequelize');
 module.exports = {
     
     read: async(req, res) => {
 
-        try {
-            
-            let data = await Spm.findAll({limit:parseInt(req.query.limit)});
-            res.status(200).json(data);
+        const {
+            query : {
+                dok_id, 
+                skpd, 
+                kepada, 
+                keperluan,
+                no_spm,
+                no_sp2d,
+                lokasi_fisik,
+                tgl_spm,
+                box,
+                is_active,
+                page,
+                limit
+            }
+        } = req;
 
+        let filter = {
+            raw: false,
+            limit: parseInt(limit),
+            offset: parseInt(limit) * (parseInt(page) - 1),
+            order: [],
+            where: {},
+        };
+
+        if (dok_id) {
+            filter.where.dok_id = { [Op.like]: `%${dok_id}%` };
+        }
+        if (lokasi_fisik) {
+            filter.where.lokasi_fisik = { [Op.like]: `%${lokasi_fisik}%` };
+        }
+        if (skpd) {
+            filter.where.skpd = { [Op.like]: `%${skpd}%` };
+        }
+        if (kepada) {
+            filter.where.kepada = { [Op.like]: `%${kepada}%` };
+        }
+        if (keperluan) {
+            filter.where.keperluan = { [Op.like]: `%${keperluan}%` };
+        }
+        if (tgl_spm) {
+            filter.where.tgl_spm = { [Op.like]: `%${tgl_spm}%` };
+        }
+        if (no_spm) {
+            filter.where.no_spm = { [Op.like]: `%${no_spm}%` };
+        }
+        if (no_sp2d) {
+            filter.where.no_sp2d = { [Op.like]: `%${no_sp2d}%` };
+        }
+        if (box) {
+            filter.where.box = box;
+        }
+        if (is_active) {
+            filter.where.is_active = is_active;
+        }
+
+        try {
+            let {count: total, rows: data} = await Spm.findAndCountAll(filter);
+            // let data = await Spj.findAll(filter);
+            res.status(200).json({
+                success: true,
+                total,
+                data
+            });
         } catch(error){
-            console.error(error);
+            res.status(200).json({
+                success: false,
+                message: 'maaf, terjadi kesalahan pada server'
+            })
         }
     },
 
