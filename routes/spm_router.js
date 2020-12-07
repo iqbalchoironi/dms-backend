f1b82b83df4b55b5c1bd1348c34f89c3a279f1bc
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-07 21:00:28 +0700

    implement upload document to spm create

--- routes/spm_router.js
+++ routes/spm_router.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { read, create, update, remove } = require('../controllers/spm_controller');
 
+const {upload} = require('../middlewares/fileUpload');
+
 router.get('/spms', read);
-router.post('/spm', create);
+router.post('/spm', upload.single('dok_file'),create);
 router.put('/spm/:id', update);
 router.delete('/spm/:id', remove);
 
