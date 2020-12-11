875a4b249a16a625330a9359688ace6cf3710ca0
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-11 16:47:58 +0700

    add fix transaction log in read document file

--- controllers/dokumen_file_controller.js
+++ controllers/dokumen_file_controller.js
@@ -1,13 +1,15 @@
 const DokFile = require('../models').dokumen_files;
 const LogActivity = require('../models').log_activity;
 const { sequelize } = require('../models');
+const { PRINT, DOCUMENT } = require('../helpers/logType');
+const moment = require('moment');
 
 module.exports = {
     sendFile: async(req, res) => {
         try {
 
             let valiableDokumen = await sequelize.transaction(async (t) => {
-                let valiableDok = await DokFile.findOne({where:{ dokumen_name: req.params.id}},{ transaction: t });
+                let valiableDokumen = await DokFile.findOne({where:{ dokumen_name: req.params.id}},{ transaction: t });
 
                 if (!valiableDokumen) {
                     return res.status(404).json({
@@ -15,17 +17,17 @@ module.exports = {
                         message: "source tidak tersedia"
                     });
                 }
-
+                let now = moment(); 
                 await LogActivity.create({
-                    fk_username:'',
-                    activity_type:'',
-                    activity_object:'',
-                    activity_object_detil:'',
-                    activity_desc:'',
-                    activity_times:'',
+                    fk_username: req.user.username,
+                    activity_type: PRINT,
+                    activity_object: DOCUMENT,
+                    activity_object_detil: valiableDokumen.dokumen_id,
+                    activity_desc: `${req.user.username} ${PRINT} ${DOCUMENT} ${valiableDokumen.dokument_id} pada ${now}`,
+                    activity_times: now,
                 },{ transaction: t});
 
-                return valiableDok;
+                return valiableDokumen;
             })
 
             let file = `${process.env.STORAGE_ROOT}${valiableDokumen.dokumen_path}/${valiableDokumen.dokumen_name}`;
