7bcfe2d312d812d7cf778e3ef6de7933e271dbb4
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-11 14:48:06 +0700

    add scema assosiation to spj

--- controllers/dokumen_file_controller.js
+++ controllers/dokumen_file_controller.js
@@ -1,23 +1,42 @@
 const DokFile = require('../models').dokumen_files;
+const LogActivity = require('../models').log_activity;
+const { sequelize } = require('../models');
 
 module.exports = {
     sendFile: async(req, res) => {
         try {
 
-            let valiableDokumen = await DokFile.findOne({where:{ dokumen_name: req.params.id}});
-        
-            if (!valiableDokumen) {
-                return res.status(404).send('gak ada sob');
-            }
+            let valiableDokumen = await sequelize.transaction(async (t) => {
+                let valiableDok = await DokFile.findOne({where:{ dokumen_name: req.params.id}},{ transaction: t });
+
+                if (!valiableDokumen) {
+                    return res.status(404).json({
+                        success: false,
+                        message: "source tidak tersedia"
+                    });
+                }
+
+                await LogActivity.create({
+                    fk_username:'',
+                    activity_type:'',
+                    activity_object:'',
+                    activity_object_detil:'',
+                    activity_desc:'',
+                    activity_times:'',
+                },{ transaction: t});
+
+                return valiableDok;
+            })
 
             let file = `${process.env.STORAGE_ROOT}${valiableDokumen.dokumen_path}/${valiableDokumen.dokumen_name}`;
-            res.status(200).sendFile(file);
+            res.status(200).download(file);
 
         }catch(error) {
-            res.status(200).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     }
 }
