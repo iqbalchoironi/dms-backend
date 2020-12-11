609bf0a3575ce99e64e6a7f29997a2ff0b5b6b61
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-11 12:57:03 +0700

    add acces api midleware with user role

--- routes/spm_router.js
+++ routes/spm_router.js
@@ -4,13 +4,14 @@ const { read, create, update, remove, exelToDB, makeReport, readById } = require
 
 const {upload} = require('../middlewares/fileUpload');
 const {uploadExel} = require('../middlewares/uploadExelToDB');
+const {verifyToken, isAdmin, isOperator} = require('../middlewares/verifyAuth');
 
-router.get('/spms', read);
-router.post('/spm', upload.single('dok_file'),create);
-router.get('/spm/report', makeReport);
-router.post('/spm/exeltodb', uploadExel.single('exel_file'),exelToDB);
-router.get('/spm/:id', readById);
-router.put('/spm/:id',upload.single('dok_file'), update);
-router.delete('/spm/:id', remove);
+router.get('/spms', verifyToken, read);
+router.post('/spm', verifyToken,isOperator, upload.single('dok_file'),create);
+router.get('/spm/report', verifyToken, makeReport);
+router.post('/spm/exeltodb',verifyToken, isOperator, uploadExel.single('exel_file'),exelToDB);
+router.get('/spm/:id',verifyToken, readById);
+router.put('/spm/:id',verifyToken, isOperator, upload.single('dok_file'), update);
+router.delete('/spm/:id', verifyToken, isAdmin, remove);
 
 module.exports = router;
