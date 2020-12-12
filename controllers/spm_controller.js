7be4ecc9a946264027031e92383a5f55ae2cfe93
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-12 21:36:34 +0700

    add transaction and log for spm endpoint

--- controllers/spm_controller.js
+++ controllers/spm_controller.js
@@ -1,10 +1,12 @@
 const Spm = require('../models').dok_spm;
 const DokFile = require('../models').dokumen_files;
+const LogActivity = require('../models').log_activity;
 const { Op } = require('sequelize');
 const {sequelize} = require('../models')
 const setId = require('../helpers/setIdDocument');
+const { CREATE, UPDATE, DELETE, PRINT, DOCUMENT } = require('../helpers/logType');
 const xlsx = require('xlsx');
-let moment = require('moment')
+const moment = require('moment')
 
 module.exports = {
     
@@ -76,10 +78,11 @@ module.exports = {
                 data
             });
         } catch(error){
-            res.status(200).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     },
 
@@ -88,19 +91,16 @@ module.exports = {
         try {
 
             let box = req.body.lokasi_fisik.split('.');
-            box = box[box.length - 1].substring(1)
+            box = box[box.length - 1];
             req.body.box = box;
 
 
             let newSpm = await sequelize.transaction(async (t) => {
 
                 if(!req.file){
-                    let count = await Spm.count() + 1;;
-                    count = '' + count;
-                    while (count.length < 8) {
-                        count = '0' + count;
-                    }
-                    req.body.dok_id =  `SPM_${count}`;
+                    let count = await Spm.count() + 1;
+                    let dokIdIndex = await setId(count);
+                    req.body.dok_id =  `SPM_${dokIdIndex}`;
                 }
                 const newSpm  = await Spm.create(req.body, { transaction: t });
 
@@ -114,24 +114,31 @@ module.exports = {
                         dokumen_file_type: req.file.mimetype
                     }, { transaction: t });
                 }
-            
+
+                let now = moment(); 
+                await LogActivity.create({
+                    fk_username: req.user.username,
+                    activity_type: CREATE,
+                    activity_object: DOCUMENT,
+                    activity_object_detil: newSpm.dok_id,
+                    activity_desc: `${req.user.username} ${CREATE} ${DOCUMENT} ${newSpm.dok_id} pada ${now}`,
+                    activity_times: now,
+                },{ transaction: t});
+
                 return newSpm;
             
             });
-
-            // newSpm  = await Spm.findOne({
-            //     include: DokFile,
-            //     where: {
-            //         dok_id: req.body.dok_id,
-            //     },
-            // });
 
             if (newSpm) {
                 res.status(200).json(newSpm);
             }
 
         }catch(error) {
-            console.log(error)
+            console.log(error);
+            res.status(500).json({
+                success: false,
+                message: 'maaf, terjadi kesalahan pada server'
+            });
         }
     },
 
@@ -190,13 +197,28 @@ module.exports = {
                         },{ transaction: t });
                     }
                 }
+
+                let now = moment(); 
+                await LogActivity.create({
+                    fk_username: req.user.username,
+                    activity_type: UPDATE,
+                    activity_object: DOCUMENT,
+                    activity_object_detil: valiableSpm.dok_id,
+                    activity_desc: `${req.user.username} ${UPDATE} ${DOCUMENT} ${valiableSpm.dok_id} pada ${now}`,
+                    activity_times: now,
+                },{ transaction: t});
+
                 return valiableSpm
             });
 
             res.status(200).json(updateSpm);
 
         }catch(error) {
-            console.log(error)
+            console.log(error);
+            res.status(500).json({
+                success: false,
+                message: 'maaf, terjadi kesalahan pada server'
+            });
         }
         
 
@@ -218,7 +240,11 @@ module.exports = {
                 res.status(200).json(valiableSpm);
 
         }catch(error) {
-            console.log(error)
+            console.log(error);
+            res.status(500).json({
+                success: false,
+                message: 'maaf, terjadi kesalahan pada server'
+            });
         }
         
 
@@ -227,23 +253,39 @@ module.exports = {
     remove: async(req, res) => {
         try {
 
-            let valiableSpm = await Spm.findOne({where:{ dok_id: req.params.id}});
+            await sequelize.transaction(async (t) => { 
+            
+                let valiableSpm =  await Spm.findOne({where:{ dok_id: req.params.id}},{ transaction: t});
         
-            if (!valiableSpm) {
-                return res.status(404).send('gak ada sob');
-            }
+                if (!valiableSpm) {
+                    return res.status(404).send('gak ada sob');
+                }
+
+                let now = moment(); 
+                await LogActivity.create({
+                    fk_username: req.user.username,
+                    activity_type: DELETE,
+                    activity_object: DOCUMENT,
+                    activity_object_detil: valiableSpm.dok_id,
+                    activity_desc: `${req.user.username} ${DELETE} ${DOCUMENT} ${valiableSpm.dok_id} pada ${now}`,
+                    activity_times: now,
+                },{ transaction: t});
     
-            await valiableSpm.destroy();
+                await valiableSpm.destroy({ transaction: t});
+            })
+
+
             res.status(200).json({
                 success: true,
                 message: 'data berhasil di hapus'
             })
 
         }catch(error) {
-            res.status(200).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     },
 
@@ -260,10 +302,11 @@ module.exports = {
             res.download(file);
 
         }catch(error) {
-            res.status(200).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     },
 
@@ -292,50 +335,52 @@ module.exports = {
             rows.shift();
             rows.shift();
 
-            let inputs = [];
             let count = await Spm.count();
-            console.log(rows.length)
-            rows.forEach(async (row) => {
-                let box = row['A'].split('.');
-                    box = box[box.length - 1];
-                
-                count++;
-                let dokIdIndex = await setId(count);
-                
-                let input = {
-                    lokasi_fisik: row['A'],
-                    skpd        : row['B'],
-                    kepada      : row['C'],
-                    keperluan   : row['D'],
-                    no_spm      : row['E'],
-                    no_sp2d     : row['F'],
-                    tgl_spm     : new moment.utc(row['G'],'DD-MM-YYYY'),
-                    fk_cat_id   : 'spm',
-                    dok_id      : `SPM_${dokIdIndex}`,
-                    box         : box
-                };
-                inputs.push(input);
-            });
-                
-            let totalInputSpm = await sequelize.transaction(async (t) => {
+            
+                rows.forEach(async (row) => {
+                    let box = row['A'].split('.');
+                        box = box[box.length - 1];
+                    
+                    count++;
+                    let dokIdIndex = await setId(count);
+                    
+                    let input = {
+                        lokasi_fisik: row['A'],
+                        skpd        : row['B'],
+                        kepada      : row['C'],
+                        keperluan   : row['D'],
+                        no_spm      : row['E'],
+                        no_sp2d     : row['F'],
+                        tgl_spm     : new moment.utc(row['G'],'DD-MM-YYYY'),
+                        fk_cat_id   : 'spm',
+                        dok_id      : `SPM_${dokIdIndex}`,
+                        box         : box
+                    };
+                    let newSpm = await Spm.create(input,{ raw: true});
 
-                let inputSpms = await Spm.bulkCreate(inputs,{ transaction: t });
-                return inputSpms
-            });
+                    let now = moment(); 
+                    await LogActivity.create({
+                        fk_username: req.user.username,
+                        activity_type: CREATE,
+                        activity_object: DOCUMENT,
+                        activity_object_detil: newSpm.dok_id,
+                        activity_desc: `${req.user.username} ${CREATE} ${DOCUMENT} ${newSpm.dok_id} pada ${now}`,
+                        activity_times: now,
+                    });
 
-                res.status(200).json({
-                    success: true,
-                    data: totalInputSpm
-                })
-            // });
+                });
 
+            res.status(200).json({
+                success: true,
+                data: 'import data telah berhasil dilakukan'
+            })
 
         }catch(error) {
-            console.log(error)
-            res.status(502).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     },
 
@@ -419,21 +464,32 @@ module.exports = {
                 },
             };
 
-            pdf.create(dataRender, options).toFile("report.pdf", function (err, data) {
+            pdf.create(dataRender, options).toFile("report.pdf", async function (err, data) {
                 if (err) {
                     res.send(err);
                 } else {
+
+                    let now = moment(); 
+                    await LogActivity.create({
+                        fk_username: req.user.username,
+                        activity_type: PRINT,
+                        activity_object: DOCUMENT,
+                        activity_object_detil: 'LAPORAN SPM',
+                        activity_desc: `${req.user.username} ${PRINT} ${DOCUMENT} SPM laporan pada ${now}`,
+                        activity_times: now,
+                    });
+
                     res.download(data.filename);
 
                 }
             });
 
         } catch(error){
-            console.log(error)
-            res.status(200).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     }
 }
