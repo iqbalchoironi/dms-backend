9c35c00aa313b453163e7c92b0d8894c41c04cd1
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-16 21:54:17 +0700

    add make report log activity

--- controllers/log_activity.js
+++ controllers/log_activity.js
@@ -50,11 +50,89 @@ module.exports = {
                 data
             });
         } catch(error){
-            res.status(200).json({
+            console.log(error);
+            res.status(500).json({
                 success: false,
                 message: 'maaf, terjadi kesalahan pada server'
-            })
+            });
         }
     },
+
+    makeReport: async (req, res) => {
+        const {
+            query : {
+                fk_username, 
+                activity_type, 
+                activity_object, 
+                activity_object_detil,
+                activity_times,
+                // page,
+                // limit,
+            }
+        } = req;
+
+        let filter = {
+            raw: false,
+            // limit: parseInt(limit),
+            // offset: parseInt(limit) * (parseInt(page) - 1),
+            order: [],
+            where: {},
+        };
+
+        if (fk_username) {
+            filter.where.fk_username = { [Op.like]: `%${fk_username}%` };
+        }
+        if (activity_type) {
+            filter.where.activity_type = activity_type;
+        }
+        if (activity_object) {
+            filter.where.activity_object = activity_object;
+        }
+        if (activity_object_detil) {
+            filter.where.activity_object_detil = { [Op.like]: `%${activity_object_detil}%` };
+        }
+        if (activity_times) {
+            filter.where.activity_times = {  [Op.between]: [new Date(activity_times), new Date(activity_times).setHours(24,0,0)] };
+        }
+
+        try {
+            let {count: total, rows: data} = await LogActivity.findAndCountAll(filter);
+            // let data = await Spj.findAll(filter);
+
+            let ejs = require("ejs");
+            let pdf = require("html-pdf");
+            let path = require("path");
+
+            let dataRender = await ejs.renderFile(path.join(__dirname,'../views/report/','log.ejs'),{data,total});
+            let options = {
+                "format": "A3",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+                "orientation": "landscape",
+                "paginationOffset": 1,
+                // "directory": '/temp',
+                "header": {
+                    "height": "10mm",
+                },
+                "footer": {
+                    "height": "10mm",
+                },
+            };
+
+            pdf.create(dataRender, options).toFile("log.pdf", function (err, data) {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.download(data.filename);
+
+                }
+            });
+
+        } catch(error){
+            console.log(error);
+            res.status(500).json({
+                success: false,
+                message: 'maaf, terjadi kesalahan pada server'
+            });
+        }
+    }
 
 }
