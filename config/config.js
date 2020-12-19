2cd15e1630213cbc5766a09d73859af4edff8c8b
iqbalchoironi <iqbal.choironi@gmail.com>
2020-12-19 13:57:41 +0700

    add configuration pool

--- config/config.js
+++ config/config.js
@@ -1,25 +1,31 @@
 require('dotenv').config(); 
 
 module.exports =  {
-  "development": {
-    "username": process.env.DB_USERNAME_DEV,
-    "password": process.env.DB_PASS_DEV,
-    "database": process.env.DB_NAME_DEV,
-    "host"    : process.env.DB_HOST_DEV,
-    "dialect" : "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+    development: {
+        username: process.env.DB_USERNAME_DEV,
+        password: process.env.DB_PASS_DEV,
+        database: process.env.DB_NAME_DEV,
+        host    : process.env.DB_HOST_DEV,
+        dialect :"mysql",
+        pool: {
+            max: 20,
+            min: 0,
+            acquire: 300000,
+            idle: 70000
+        }
+    },
+    test: {
+        username:"root",
+        password: null,
+        database:"database_test",
+        host:"127.0.0.1",
+        dialect:"mysql"
+    },
+    production: {
+        username:root,
+        password: null,
+        database:"database_production",
+        host:"127.0.0.1",
+        dialect:"mysql"
+    }
 }
